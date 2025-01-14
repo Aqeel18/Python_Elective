a4c619761223cb6086daf9d0e542ef179f0f7e0f
Aqeel18 <contactaqeel007@gmail.com>
2025-01-14 21:31:16 +0530

    investment_report

---
+++ investment_report.py
@@ -0,0 +1,4 @@
+invest = float(input("Enter investment amount: "))
+ret_amt = float(input("Enter the amount of the return: "))
+returnn = (ret_amt - invest)/invest
+print("Investment return is", returnn)
